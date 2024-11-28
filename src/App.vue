36920b17cba674fc1d29c7444738a1999ef44ed5
Eric Freitas <freitaseric@proton.me>
2024-11-27 20:23:48 -0400

    chore: project setup

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="container">
+    <h1>TrueTasks</h1>
+  </main>
+</template>
+
+<style>
+:root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+
+  color: #0f0f0f;
+  background-color: #f6f6f6;
+
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    color: #f6f6f6;
+    background-color: #2f2f2f;
+  }
+}
+</style>
